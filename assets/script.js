ed949161a1d81fa28f90ce7583eb981dc45419e4
Reuben Dawe <reubendawemarketing@gmail.com>
2024-12-17 19:03:27 +0000

    Created hero section, added various colours

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,11 @@ const /* {NodeElement} */ $navbar = document.querySelector("[data-navbar]");
 const /* {NodeElement} */ $navToggler = document.querySelector("[data-nav-toggler]");
 
 $navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+/* Header scroll state */
+
+const /* {NodeElement} */ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
